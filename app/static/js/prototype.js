f974108f7220ec712b4c83369389d59236b42f5e
Fiona Rowan <fmr2112@columbia.edu>
2015-09-27 13:01:20 -0400

    placeholder images and integrated js

--- app/static/js/prototype.js
+++ app/static/js/prototype.js
@@ -1,14 +1,27 @@
 window.onload = function() {
   	document.querySelectorAll('p')[0].classList.add('text-load');
+  	  			$("#stage").hide();
+  	  			$("#button").click(function() { 
+    // assumes element with id='button'
+    $("#container").toggle();
+    $("#stage").toggle();
+    });
   	
   	var callback = function() {
   		var btn = document.querySelectorAll('button')[0];
   		btn.classList.add('text-load');
   		btn.addEventListener('click', function() {
   			document.location = 'videoPage.html';
+
   		});
+  		
   	};
   	setTimeout(callback, 1250);
   	
+  	
+  	
   
-}
+};
+
+
+  	
